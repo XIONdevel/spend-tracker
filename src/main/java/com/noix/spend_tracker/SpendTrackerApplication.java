b705af0ab9788c0c8d543e7df2f0eff102043fd0
XIONdevel <igor97645@gmail.com>
2025-04-27 21:24:22 +0300

    init commit

---
+++ src/main/java/com/noix/spend_tracker/SpendTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.noix.spend_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noix/spend_tracker/SpendTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noix.spend_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
